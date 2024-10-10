9759bc97b7e2cb6ab80d953742055603d24d4fb2
YeyouminTian <will.tianye@outlook.com>
2024-10-10 19:26:31 +0800

    修改常住地计算逻辑

--- SQL/3.1.2计算用户常住地.sql
+++ SQL/3.1.2计算用户常住地.sql
@@ -4,17 +4,11 @@ CREATE TABLE IF NOT EXISTS nc_3_1_2_user_longest_stay_base_station (
     "LONGITUDE" FLOAT8,
     "LATITUDE" FLOAT8,
     "OCCURRENCE_COUNT" INT,
-    "TOTAL_DAYS" INT,
     "PERCENTAGE" FLOAT8
 );
 
 -- 插入数据
-WITH user_total_days AS (
-    SELECT "USERID", COUNT(DISTINCT "DATE") AS total_days
-    FROM nc_2_1_3_1_60min_user_night_longest_stay
-    GROUP BY "USERID"
-),
-user_stays AS (
+WITH user_stays AS (
     SELECT 
         "USERID",
         "LONGITUDE",
@@ -28,13 +22,10 @@ user_stays AS (
 user_max_stays AS (
     SELECT 
         us.*,
-        utd.total_days,
-        CAST(us.occurrence_count AS FLOAT) / NULLIF(utd.total_days, 0) AS percentage,
+        CAST(us.occurrence_count AS FLOAT) / 7 AS percentage,
         ROW_NUMBER() OVER (PARTITION BY us."USERID" ORDER BY us.occurrence_count DESC) AS rn
     FROM 
         user_stays us
-    JOIN
-        user_total_days utd ON us."USERID" = utd."USERID"
 )
 INSERT INTO nc_3_1_2_user_longest_stay_base_station
 SELECT 
@@ -42,7 +33,6 @@ SELECT
     "LONGITUDE",
     "LATITUDE",
     occurrence_count AS "OCCURRENCE_COUNT",
-    total_days AS "TOTAL_DAYS",
     percentage AS "PERCENTAGE"
 FROM 
     user_max_stays
